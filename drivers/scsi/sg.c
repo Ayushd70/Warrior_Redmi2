6172d79f0193bb1f3ed7c896ae60e871d33c18c0
Jens Axboe <axboe@fb.com>
2014-06-06 07:57:37 -0600

    BACKPORT: block: add blk_rq_set_block_pc()
    With the optimizations around not clearing the full request at alloc
    time, we are leaving some of the needed init for REQ_TYPE_BLOCK_PC
    up to the user allocating the request.
    
    Add a blk_rq_set_block_pc() that sets the command type to
    REQ_TYPE_BLOCK_PC, and properly initializes the members associated
    with this type of request. Update callers to use this function instead
    of manipulating rq->cmd_type directly.
    
    Includes fixes from Christoph Hellwig <hch@lst.de> for my half-assed
    attempt.
    
    Change-Id: Ifc386dfb951c5d6adebf48ff38135dda28e4b1ce
    Signed-off-by: Jens Axboe <axboe@fb.com>
    Signed-off-by: RahifM <faizel326@gmail.com>

--- drivers/scsi/sg.c
+++ drivers/scsi/sg.c
@@ -1683,9 +1683,7 @@ static int sg_start_req(Sg_request *srp, unsigned char *cmd)
 	if (hp->cmd_len > BLK_MAX_CDB)
 		rq->cmd = long_cmdp;
 	memcpy(rq->cmd, cmd, hp->cmd_len);
-
 	rq->cmd_len = hp->cmd_len;
-	rq->cmd_type = REQ_TYPE_BLOCK_PC;
 
 	srp->rq = rq;
 	rq->end_io_data = srp;
