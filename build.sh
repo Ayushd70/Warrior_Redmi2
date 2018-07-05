c3abe690854f4b5efe659ecb3e8317e42eeaa5ee
RahifM <faizel326@gmail.com>
2018-07-05 20:01:16 +0530

    build: cc: We use GCC-7.x now
    Signed-off-by: RahifM <faizel326@gmail.com>

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 # Modify the following variable if you want to build
-export CROSS_COMPILE="$HOME/android/kernels/toolchains/arm-4.9/bin/arm-linux-androideabi-"
+export CROSS_COMPILE="$HOME/android/kernels/toolchains/arm-lin-7/bin/arm-linux-gnueabi-"
 export ARCH=arm
 export SUBARCH=arm
 make wt88047_defconfig
